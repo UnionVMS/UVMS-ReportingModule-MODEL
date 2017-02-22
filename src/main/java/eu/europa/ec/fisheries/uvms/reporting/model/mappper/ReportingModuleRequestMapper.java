840e2bc8ac1e9f8fabbddfeea7f03b3f23e9f859
rinaldgr <ZMuFAz0141>
2017-02-22 16:21:37 +0100

    * moved JaxBMarshaller in model again

--- src/main/java/eu/europa/ec/fisheries/uvms/reporting/model/mappper/ReportingModuleRequestMapper.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/reporting/model/mappper/ReportingModuleRequestMapper.java
@@ -13,6 +13,7 @@ package eu.europa.ec.fisheries.uvms.reporting.model.mappper;
 import eu.europa.ec.fisheries.uvms.reporting.model.exception.ReportingModelException;
 import eu.europa.ec.fisheries.uvms.reporting.model.schemas.ReportGetStartAndEndDateRQ;
 import eu.europa.ec.fisheries.uvms.reporting.model.schemas.ReportingModuleMethod;
+import eu.europa.ec.fisheries.uvms.reporting.model.util.JAXBMarshaller;
 
 /**
  * Created by padhyad on 3/18/2016.
